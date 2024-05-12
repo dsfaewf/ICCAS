71f7009ebaa58cde67a7defc308aa3226deba896
MuHaRVEY <rkddntjr0123@naver.com>
2024-05-12 16:31:35 +0900

    파이어베이스 Realtime Database에 내용 넣는 부분 부분 코드 추가

--- app/src/main/java/com/example/testfolder/MainActivity3.kt
+++ app/src/main/java/com/example/testfolder/MainActivity3.kt
@@ -3,17 +3,24 @@ package com.example.testfolder
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity3 : AppCompatActivity() {
+    private lateinit var databaseReference: DatabaseReference // 변경된 부분
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
 
         val diaryEditText = findViewById<EditText>(R.id.diary_edit_text)
         val characterCountTextView = findViewById<TextView>(R.id.character_count_text_view)
+
+        // Firebase 데이터베이스 루트 참조 가져오기
+        var databaseReference = FirebaseDatabase.getInstance().reference.child("diaries")
 
         diaryEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
@@ -25,5 +32,14 @@ class MainActivity3 : AppCompatActivity() {
                 characterCountTextView.text = "$count/1000"
             }
         })
+        val saveButton = findViewById<Button>(R.id.my_button6)
+        saveButton.setOnClickListener {
+            val diaryContent = diaryEditText.text.toString()
+            // Firebase 데이터베이스에 데이터 업로드
+            databaseReference.push().setValue(diaryContent)
+            // 또는 원하는 구조로 데이터를 저장할 수 있습니다.
+            // databaseReference.child("user_id").child("diary_id").setValue(diaryContent);
+        }
+
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,6 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     id("org.jetbrains.kotlin.android") version "2.0.0-RC2" apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
+
 }
