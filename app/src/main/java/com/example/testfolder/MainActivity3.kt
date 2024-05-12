3baea16a47de62b03eaccd726bf4f0dcff6db634
dsfaewf <simjh3808@gmail.com>
2024-05-12 15:45:55 +0900

    test

--- app/src/main/java/com/example/testfolder/MainActivity3.kt
+++ app/src/main/java/com/example/testfolder/MainActivity3.kt
@@ -1,11 +1,29 @@
 package com.example.testfolder
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+        val diaryEditText = findViewById<EditText>(R.id.diary_edit_text)
+        val characterCountTextView = findViewById<TextView>(R.id.character_count_text_view)
+
+        diaryEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                val count = s?.length ?: 0
+                characterCountTextView.text = "$count/1000"
+            }
+        })
     }
 }
