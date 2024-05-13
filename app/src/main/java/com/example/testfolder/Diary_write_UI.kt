864e317eb37db51eb8fea9e8913d9937de8d75e8
dsfaewf <simjh3808@gmail.com>
2024-05-13 12:34:57 +0900

    다이어리부분 firebase 및 날짜 나오게 하는 save버튼 리스너 병합함

--- app/src/main/java/com/example/testfolder/Diary_write_UI.kt
+++ app/src/main/java/com/example/testfolder/Diary_write_UI.kt
@@ -13,19 +13,19 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class Diary_write_UI : AppCompatActivity() {
-    private lateinit var databaseReference: DatabaseReference // 변경된 부분
+    private lateinit var databaseReference: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main3) // 여기에 setContentView 추가
+        setContentView(R.layout.activity_main3)
 
         val saveButton = findViewById<Button>(R.id.my_button6)
         val diaryEditText = findViewById<EditText>(R.id.diary_edit_text)
-
-        // Firebase 데이터베이스 루트 참조 가져오기
-        databaseReference = FirebaseDatabase.getInstance().reference.child("diaries") // 변경된 부분
-
         val characterCountTextView = findViewById<TextView>(R.id.character_count_text_view)
         val dateTextView = findViewById<TextView>(R.id.date_text_view)
+
+        // Firebase 데이터베이스 루트 참조 가져오기
+        databaseReference = FirebaseDatabase.getInstance().reference.child("diaries")
 
         // Save 버튼 클릭 시 날짜 표시
         saveButton.setOnClickListener {
@@ -33,26 +33,21 @@ class Diary_write_UI : AppCompatActivity() {
             val currentDate = getCurrentDate()
             // 날짜를 TextView에 설정
             dateTextView.text = currentDate
+
+            // 일기 내용을 Firebase 데이터베이스에 업로드
+            val diaryContent = diaryEditText.text.toString()
+            databaseReference.push().setValue(diaryContent)
         }
 
         // 텍스트 입력 수를 표시
         diaryEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-
             override fun afterTextChanged(s: Editable?) {
                 val count = s?.length ?: 0
                 characterCountTextView.text = "$count/1000"
             }
         })
-        saveButton.setOnClickListener {
-            val diaryContent = diaryEditText.text.toString()
-            // Firebase 데이터베이스에 데이터 업로드
-            databaseReference.push().setValue(diaryContent)
-            // 또는 원하는 구조로 데이터를 저장할 수 있습니다.
-            // databaseReference.child("user_id").child("diary_id").setValue(diaryContent);
-        }
     }
 
     // 현재 날짜를 가져오는 함수
