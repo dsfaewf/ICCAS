4aa4e32ffb8e4935cca17aec0ee0b982bb883a7d
MuHaRVEY <rkddntjr0123@naver.com>
2024-06-20 14:23:29 +0900

    버전 충돌로 인해 어플이 실행이 되지 않아copileSdk와 targetSdk 버전을 35로 수정하였습니다.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.testfolder"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.testfolder"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
