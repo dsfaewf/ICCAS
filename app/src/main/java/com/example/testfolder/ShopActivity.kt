85ff9545c32995780b41fe3c4e52e50d71d7d4ee
Yoon Byul <yb97pe@gmail.com>
2024-07-02 23:55:04 +0900

    update coin text view

--- app/src/main/java/com/example/testfolder/ShopActivity.kt
+++ app/src/main/java/com/example/testfolder/ShopActivity.kt
@@ -1,6 +1,7 @@
 package com.example.testfolder
 
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -18,10 +19,13 @@ class ShopActivity : AppCompatActivity(), ShopItemsAdapter.OnItemClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop)
 
+        var coin = findViewById<TextView>(R.id.coin_text)
         val catGif = findViewById<GifImageView>(R.id.cat_gif)
         // GIF 반복 설정
         val gifDrawable = catGif.drawable as GifDrawable
         gifDrawable.loopCount = 0 // 무한 반복
+
+        coin.setText("1000")    // user coin 개수
 
         recyclerView = findViewById(R.id.shop_items_recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
