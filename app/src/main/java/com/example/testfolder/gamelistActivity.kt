eead69e4bd20eec235bd94abf02a71a4d8ec37d6
Yoon Byul <yb97pe@gmail.com>
2024-07-06 00:52:12 +0900

    update game selection page

--- app/src/main/java/com/example/testfolder/gamelistActivity.kt
+++ app/src/main/java/com/example/testfolder/gamelistActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class gamelistActivity : AppCompatActivity() {
@@ -11,12 +12,12 @@ class gamelistActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gamelist)
 
-        val btn1 = findViewById<Button>(R.id.btn1)
-        val btn2 = findViewById<Button>(R.id.btn2)
-        val btn3 = findViewById<Button>(R.id.btn3)
-        val btn4 = findViewById<Button>(R.id.btn4)
-        val btn5=findViewById<Button>(R.id.btn5)
-        val btn6=findViewById<Button>(R.id.btn6)
+        val btn1 = findViewById<LinearLayout>(R.id.btn1)
+        val btn2 = findViewById<LinearLayout>(R.id.btn2)
+        val btn3 = findViewById<LinearLayout>(R.id.btn3)
+        val btn4 = findViewById<LinearLayout>(R.id.btn4)
+        val btn5 = findViewById<LinearLayout>(R.id.btn5)
+        val btn6 = findViewById<LinearLayout>(R.id.btn6)
 
         btn1.setOnClickListener {
             val intent = Intent(this, gameLowActivity::class.java)
