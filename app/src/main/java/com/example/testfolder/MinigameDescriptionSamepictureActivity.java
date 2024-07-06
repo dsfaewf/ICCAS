3187b13a074d937d737217781dfbac72399b7227
dsfaewf <simjh3808@gmail.com>
2024-07-06 18:39:31 +0900

    미니게임 설명 페이지 제작 및 연결

---
+++ app/src/main/java/com/example/testfolder/MinigameDescriptionSamepictureActivity.java
@@ -0,0 +1,49 @@
+package com.example.testfolder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MinigameDescriptionSamepictureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_minigame_description_samepicture);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // 뒤로가기 버튼 클릭 리스너 설정
+        Button backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MinigameDescriptionSamepictureActivity.this, gamelistActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        // 게임 시작하기 버튼 클릭 리스너 설정
+        Button startButton = findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MinigameDescriptionSamepictureActivity.this, ServeGame_samepicture.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
